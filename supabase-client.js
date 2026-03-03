0bb0a528c2ab7ad1484e9f9ce764643f7f85e1cc
adrianarrazabal152003-blip <adrianarrazabal152003@gmail.com>
2026-03-03 16:48:09 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://yshzobskmhuxheerziue.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzaHpvYnNrbWh1eGhlZXJ6aWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODIzNzUsImV4cCI6MjA4ODA1ODM3NX0.N0IOynFCTL25LtbmKP48jIadYGFDFEl-IyotKWbxPwY';
 
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
